2ff77ec1985c827a144b5b6ed0c23073fd96c4c0
Darkson Bruno <darksonbruno2001.net@gmail.com>
2024-01-14 15:30:40 +0000

    commit inicial

---
+++ styles.js
@@ -0,0 +1,23 @@
+let person = {
+    name : 'Darkson',
+    age : 22,
+    adress : 'Rua Praceta Manuel Alvares',
+    email : 'darksonbruno2001.net@gmail.com',
+    number : '966 303 322',
+    job : 'barber',
+    amigavel : true,
+
+}
+
+let array = {
+    hobby : ['soccer','music','movies','travel',]
+    
+}
+console.log (person)
+console.log (array)
+console.log(person.name + ' likes to play '+ array.hobby[0]);
+
+let automovel = {
+    marca : "bmw",
+    
+}
